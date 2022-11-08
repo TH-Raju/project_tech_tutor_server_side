12fa1bf7fad3b5a13cd7e577c1ecdbda2c88ef0d
TH-Raju <rajukhan894200@gmail.com>
2022-11-08 22:27:24 +0600

    mongodb added

--- index.js
+++ index.js
@@ -2,10 +2,29 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 const app = express();
+require('dotenv').config();
 const port = process.env.PORT || 5000;
 
 
 app.use(cors());
+app.use(express.json());
+
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ddpko0x.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+async function run() {
+    try {
+
+    }
+    finally {
+
+    }
+}
+run().catch(err => console.error(err));
 
 
 app.get('/', (req, res) => {
