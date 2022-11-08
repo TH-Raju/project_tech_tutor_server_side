a842191c8106edc215f8dc764b09bae06bf88bcd
TH-Raju <rajukhan894200@gmail.com>
2022-11-09 01:29:29 +0600

    Review section API set up complete

--- index.js
+++ index.js
@@ -11,13 +11,46 @@ app.use(express.json());
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ddpko0x.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 
 async function run() {
     try {
+        const serviceCollection = client.db('myService').collection('services');
+        const userFeedback = client.db('myService').collection('review');
+
+        app.get('/services', async (req, res) => {
+            const query = {}
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services);
+        });
+
+
+        app.get('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await serviceCollection.findOne(query);
+            res.send(result);
+        });
+
+        //Review / feedback
+
+        app.post('/review', async (req, res) => {
+            const review = req.body;
+            const usrfeedback = await userFeedback.insertOne(review);
+            res.send(usrfeedback);
+        })
+
+        app.get('/review', async (req, res) => {
+            const query = {}
+            const cursor = userFeedback.find(query);
+            const feedback = await cursor.toArray();
+            res.send(feedback);
+        });
+
 
     }
     finally {
